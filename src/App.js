d473e9a526f2de34bc1cf2859a3b3f55c5523889
N Bizzell <bizzbizzell@gmail.com>
2020-08-27 11:56:11 +0100

    App created

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Login from './Login'
+import Dashboard from './Dashboard';
+import { ApiClient } from './apiClient';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      token: window.localStorage.getItem("token")
+    }
+    this.client = new ApiClient(() => this.state.token, () => this.logout())
+  }
+
+  login(token) {
+    window.localStorage.setItem("token", token)
+    this.setState({ token })
+  }
+
+  logout() {
+    window.localStorage.removeItem("token")
+    this.setState({ token: undefined })
+  }
+
+
+  render() {
+    if (this.state.token) {
+      console.log("dashboard")
+      return <Dashboard client={this.client}/>
+    }
+    console.log("login")
+    return <Login loggedIn={(token) => this.login(token)} client={this.client}/>
+  }
+}
+
+export default App;
